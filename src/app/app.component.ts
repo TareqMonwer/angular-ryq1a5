6842277a7e767e61e3295a86eabea63aadc7af06
TareqMonwer <tareqmonwer137@gmail.com>
2022-06-21 13:33:08 +0600

    take practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -20,10 +20,13 @@ export class AppComponent implements OnInit {
     //   complete: () => console.log("completed....")
     // })
 
-    of(1, 2, 3, 4, 5, 6).pipe(
-      tap(item => console.log(item, ' first state')),
-      map(item => item * 2),
-      tap(item => console.log(item, ' second state'))
-    ).subscribe()
+    of(1, 2, 3, 4, 5, 6)
+      .pipe(
+        map((item) => item * 2),
+        // tap(item => console.log(item, ' second state')),
+        take(2),
+        map((item) => console.log(item, 'after take'))
+      )
+      .subscribe();
   }
 }
